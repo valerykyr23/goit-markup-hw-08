a70658d45df0c72a33d73093e43cf09e999083ca
Valeriia Kyrychenko <valeriia.kyrychenko22@gmail.com>
2022-05-04 05:02:01 +0300

    Добавляет код для десктоп

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,9 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-
+  const htmlRef = document.querySelector("html");
+  const bodyRef = document.querySelector("body");
+  
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
@@ -10,5 +12,7 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
+    htmlRef.classList.toggle("overflow-y-hidden");
+    bodyRef.classList.toggle("overflow-y-hidden");
   });
 })();
